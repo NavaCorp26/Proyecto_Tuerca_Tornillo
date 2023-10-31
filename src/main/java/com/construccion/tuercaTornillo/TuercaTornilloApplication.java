b478d58ca59fc784743df45d03fd785db977800b
NavaCorp26 <luisnavarrete1a89@gmail.com>
2023-10-31 09:44:43 -0500

    new file:   .gitignore new file:   nbactions.xml new file:   pom.xml new file:   src/main/java/com/construccion/tuercaTornillo/ServletInitializer.java new file:   src/main/java/com/construccion/tuercaTornillo/TuercaTornilloApplication.java new file:   src/main/resources/application.properties new file:   src/test/java/com/construccion/tuercaTornillo/TuercaTornilloApplicationTests.java

---
+++ src/main/java/com/construccion/tuercaTornillo/TuercaTornilloApplication.java
@@ -0,0 +1,13 @@
+package com.construccion.tuercaTornillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuercaTornilloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuercaTornilloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/construccion/tuercaTornillo/TuercaTornilloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construccion.tuercaTornillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuercaTornilloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
